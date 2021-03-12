1db635068407a7df74573da2fd7155065a135d51
Rory Haggart <rdeh10@googlemail.com>
2021-03-12 11:21:33 +0000

    malaga on vins execution files

--- Malaga/VINS-Mono_Files/malaga_VINS-Mono.py
+++ Malaga/VINS-Mono_Files/malaga_VINS-Mono.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 16 09:17:20 2021
+Created on Fri Mar 12 09:51:01 2021
 
 @author: rory_haggart
 
 BRIEF:  This script self-contains the full process of running a segment of 
-        the midair dataset on the VI-SLAM algorithm, VINS-Mono. 
+        the malaga dataset on the VI-SLAM algorithm, VINS-Mono. 
 
         The script requires a single input argument, and this is the full 
         path to a .bag file of a midair segment 
-        (e.g. [ROOT]/MidAir/Kite_test/sunny/trajectory_0001_color_down.bag)
+        (e.g. [ROOT]/Malaga/malaga_15/malaga_15.bag)
 
 The script takes this bag file and passes it to the .sh file which handles
 the VINS-Mono process. This includes building the workspace, launching the 
@@ -24,62 +24,54 @@ TODO:
     Add input argument to .sh file as the .bag
     Add a 'start' and 'end' marker onto the x/y graphs?
 """
-import h5py    # to read .hdf5 sensor records files
 import matplotlib.pyplot as plt
 import numpy as np
 import re
 import sys
 import subprocess
+import os
+import shlex
 
-def main(arg1):
+def main():
     # the input argument is the path to the bag file that is being run in this test
-    bagFilePath = arg1
-    # example: "/media/rory_haggart/ENDLESS_BLU/SLAM_datasets/MidAir/MidAir/Kite_test/sunny/trajectory_0001_color_down.bag" 
+    bagFilePath = "/media/rory_haggart/ENDLESS_BLU/SLAM_datasets/RoryHaggart_SceneVisibilityInSLAM/Malaga/Malaga/malaga_15/malaga_15.bag"
+    #arg1
+
+    shellFile = os.path.abspath(os.getcwd() + "/malaga_VINS-Mono.sh")
     
-    subprocess.run(["chmod", "+x", "/media/rory_haggart/ENDLESS_BLU/SLAM_datasets/MidAir/midair_VINS-Mono.sh"], shell=True, executable='/bin/bash')
-    subprocess.run("/media/rory_haggart/ENDLESS_BLU/SLAM_datasets/MidAir/midair_VINS-Mono.sh", shell=True, executable='/bin/bash')
+    subprocess.run(["chmod", "+x", shellFile], shell=True, executable='/bin/bash')
+    subprocess.run(shellFile, shell=True, executable='/bin/bash')
     
     # TODO: if .bag doesn't exist, prompt to create it?
     # TODO: add a 'show graphs' option to the call
     
-    # the environment of the simulation (kite, ple, vo; each _test or _training)
-    environment = re.search("MidAir/MidAir/(.*?)/", bagFilePath).group(1)
-    # the weather/season we're interested in 
-    condition = re.search(environment + "/(.*?)/", bagFilePath).group(1) 
-    # the particular trajectory number
-    trajectory = re.search("trajectory_(.*?)_", bagFilePath).group(1)
-    # the camera that we're using
-    camera = re.search(trajectory + "_(.*)\.", bagFilePath).group(1) 
+    # get the folder containing all the data
+    extractFolder = os.path.abspath(os.path.dirname(bagFilePath))
+    extractNumber = extractFolder[-2:]      
+    # get the file with the GPS (ground truth) data            
+    GPSFile = extractFolder + "/malaga-urban-dataset-extract-{}_all-sensors_GPS.txt".format(extractNumber)
     
-    # define the path to the folder containing our sensor records
-    sensorRecordsPath = '/media/rory_haggart/ENDLESS_BLU/SLAM_datasets/MidAir/MidAir/' + environment + '/' + condition
+    # read the collected GPS data into a list
+    with open(GPSFile) as f1:
+        GPSDataFile = f1.readlines()
+    
+    # split the list items by spaces
+    GPSData = []
+    for line in GPSDataFile:
+        GPSData.append(shlex.split(line))
 
-    # open sensor_records.hdf5 (for ground truth info)
-    f1 = h5py.File(sensorRecordsPath + '/sensor_records.hdf5','r+')   
-    
-    # get the group for the ground truth records
-    groundTruth = f1['trajectory_' + trajectory]['groundtruth']
-    
+    # get ground truth timestamps
+    timestampGT = [float(row[0]) for row in GPSData[1:]]
+
     poseGT = [None] * 3 # initialise a list for the ground truth pose graphs
     
     # get the position data and extract x, y, z
-    poseGraphGroundTruth = list(groundTruth['position'])
-    poseGT[0] = [row[0] for row in poseGraphGroundTruth] # x
-    poseGT[1] = [row[1] for row in poseGraphGroundTruth] # y
-    poseGT[2] = [row[2] for row in poseGraphGroundTruth] # z
-    
-    groundTruthRate = 100   # the ground truth data is logged at 100Hz
-    
-    # create a time series that is the length of the data so we can align 
-    # the ground truth with the estimated pose graph
-    timestampGT = np.arange(0, len(poseGraphGroundTruth)/groundTruthRate, 1/groundTruthRate).tolist()
-    
-    # rounding etc. can cause timestamp array to be an element too long
-    if len(timestampGT) > len(poseGraphGroundTruth):
-        timestampGT = timestampGT[0:len(poseGraphGroundTruth)]
+    poseGT[0] = [float(row[8]) for row in GPSData[1:]]  # x
+    poseGT[1] = [float(row[9]) for row in GPSData[1:]]  # y
+    poseGT[2] = [float(row[10]) for row in GPSData[1:]] # z
     
     # the .sh script writes the pose estimate to this .txt file
-    poseGraphPath = "/media/rory_haggart/ENDLESS_BLU/SLAM_datasets/MidAir/poseGraph.txt"
+    poseGraphPath = os.path.abspath(os.getcwd() + "/poseGraph.txt")
     
     # open the .txt file and grab the lines of output from rostopic
     with open(poseGraphPath, "r") as f2:
@@ -96,11 +88,12 @@ def main(arg1):
     poseEst = [None] * 3 # initialise a list for the estimated pose graphs
     
     # format: (0)time, (1)seq, (2)stamp, (3)frame_id, (4)child_frame_id, (5)x, (6)y, (7)z, (8)orientation.x,orientation.y,orientation.z,orientation.w,covariance0,field.pose.covariance1,field.pose.covariance2,field.pose.covariance3,field.pose.covariance4,field.pose.covariance5,field.pose.covariance6,field.pose.covariance7,field.pose.covariance8,field.pose.covariance9,field.pose.covariance10,field.pose.covariance11,field.pose.covariance12,field.pose.covariance13,field.pose.covariance14,field.pose.covariance15,field.pose.covariance16,field.pose.covariance17,field.pose.covariance18,field.pose.covariance19,field.pose.covariance20,field.pose.covariance21,field.pose.covariance22,field.pose.covariance23,field.pose.covariance24,field.pose.covariance25,field.pose.covariance26,field.pose.covariance27,field.pose.covariance28,field.pose.covariance29,field.pose.covariance30,field.pose.covariance31,field.pose.covariance32,field.pose.covariance33,field.pose.covariance34,field.pose.covariance35,field.twist.twist.linear.x,field.twist.twist.linear.y,field.twist.twist.linear.z,field.twist.twist.angular.x,field.twist.twist.angular.y,field.twist.twist.angular.z,field.twist.covariance0,field.twist.covariance1,field.twist.covariance2,field.twist.covariance3,field.twist.covariance4,field.twist.covariance5,field.twist.covariance6,field.twist.covariance7,field.twist.covariance8,field.twist.covariance9,field.twist.covariance10,field.twist.covariance11,field.twist.covariance12,field.twist.covariance13,field.twist.covariance14,field.twist.covariance15,field.twist.covariance16,field.twist.covariance17,field.twist.covariance18,field.twist.covariance19,field.twist.covariance20,field.twist.covariance21,field.twist.covariance22,field.twist.covariance23,field.twist.covariance24,field.twist.covariance25,field.twist.covariance26,field.twist.covariance27,field.twist.covariance28,field.twist.covariance29,field.twist.covariance30,field.twist.covariance31,field.twist.covariance32,field.twist.covariance33,field.twist.covariance34,field.twist.covariance35']
-    timestampEst = [(float(row[0])*pow(10,-9) - 100000) for row in poseGraphEstimate]
+    timestampEst = [(float(row[0])*pow(10,-9)) for row in poseGraphEstimate]
     poseEst[0] = [float(row[5]) for row in poseGraphEstimate]
     poseEst[1] = [float(row[6]) for row in poseGraphEstimate]
     poseEst[2] = [float(row[7]) for row in poseGraphEstimate]
     
+    """
     # add cushions to the timestamps to keep the est and GT the same length    
     minCushion = np.linspace(0, min(timestampEst), 10).tolist()
     maxCushion = np.linspace(max(timestampEst), max(timestampGT), 10).tolist()
@@ -132,6 +125,7 @@ def main(arg1):
     poseEst[1] = [el*ySign for el in poseEst[1]]
     poseEst[2] = [el*zSign for el in poseEst[2]]
     
+    
     evenlySpacedVals = lambda m, n: [i*n//m + n//(2*m) for i in range(m)]
     
     numVals = 500
@@ -145,7 +139,7 @@ def main(arg1):
         poseEst_idxList = evenlySpacedVals(numVals, len(poseEst[axisIdx]))
         poseEst[axisIdx] = [poseEst[axisIdx][i] for i in poseEst_idxList]
 
-    
+    """
     #timeStartLineStyle = {"x": poseGraphTimestamp[1], "color": "red", "linestyle": "dashed", "linewidth": 0.5}
     
     ### PLOTS ###
@@ -163,7 +157,7 @@ def main(arg1):
     for plotIdx in range(3):
         axs[plotIdx][1].plot(timestampEst, poseEst[plotIdx % 3])
         axs[plotIdx][1].grid()
-        axs[plotIdx][1].set_xlim(left=0)
+        #axs[plotIdx][1].set_xlim(left=0)
         axs[plotIdx][1].set_ylim([min(min(poseGT[plotIdx % 3]), min(poseEst[plotIdx % 3])), max(max(poseGT[plotIdx % 3]), max(poseEst[plotIdx % 3]))])
     
     # set the various titles and axis labels
@@ -228,9 +222,7 @@ def main(arg1):
     print("Error Cost X: {}".format(SSE[0]))
     print("Error Cost Y: {}".format(SSE[1]))
     print("Error Cost Z: {}".format(SSE[2]))
-    
-    f1.close()  # close the .hdf5 file we opened
-    
+
     discontinuityDetect = np.where(abs(np.diff(np.array(poseEst[2])))>2)[0] + 1
     
     # scores can be artifically low if the estimator reset during tracking, as
@@ -243,4 +235,14 @@ def main(arg1):
     return(SSE)
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main()
+    
+"""    
+def rotate_origin_only(xy, radians):
+    #Only rotate a point around the origin (0, 0).
+    x, y = xy
+    xx = x * math.cos(radians) + y * math.sin(radians)
+    yy = -x * math.sin(radians) + y * math.cos(radians)
+
+    return xx, yy
+"""
