b3a704476076a3f889a96676da2af544d4095243
Rory Haggart <rdeh10@googlemail.com>
2021-04-09 14:33:51 +0100

    trying to align vivid to ground truth

--- ViViD/ORB-SLAM2_Files/vivid_ORB-SLAM2.py
+++ ViViD/ORB-SLAM2_Files/vivid_ORB-SLAM2.py
@@ -53,22 +53,36 @@ def main(*args):
     # run the shell file with the selected bag file as an input argument
     subprocess.run(shellFile + " " + bagFilePath, shell=True, executable='/bin/bash')
     
-    # get the trajectory number from the provided .bag file name
-    trajectory = re.search(".*/(.*?).bag", bagFilePath).group(1)
-    
+    # read the bag file so we can access some of the info
     bagFile = bagreader(bagFilePath)
     
-    # the ground truth file starts from 0.0
+    # the ground truth time is relative to start, but we would like to know
+        # the timestamp of the first image to align trajectories
         # to get the actual start time since epoch, look at the timestamps of
         # the .bag file messages. could get /rgb/image but thats a *lot* of
         # data, so query the info topic instead which is published at the same time
     imgmsgs = bagFile.message_by_topic('/rgb/camera_info')
     df = pd.read_csv(imgmsgs)
-    # clean up the generated files
+    
+    # we should also rotate the estimation to match the ground truth. 
+        # attitude is not contained in ground truth but the on-board IMU does
+        # record it, so get the IMU data to extract this info later
+    imumsgs = bagFile.message_by_topic('/imu/data')
+    df2 = pd.read_csv(imumsgs)
+    
+    # clean up the generated csv files now that we have the data
     os.remove(imgmsgs)
+    os.remove(imumsgs)
     os.rmdir(os.path.dirname(imgmsgs))
     
-    # define the path to the folder containing our sensor records
+    # save the time stamp of the first image to subtract from the estimate time
+    startTimeIMG = df['header.stamp.secs'][0] + pow(10,-9) * df['header.stamp.nsecs'][0]
+    # now store the image timestamps in a list starting from 0.0
+    timestampIMG = list(df['header.stamp.secs'] + pow(10,-9) * df['header.stamp.nsecs'] - startTimeIMG)
+    
+    # get the trajectory name from the provided .bag file name
+    trajectory = re.search(".*/(.*?).bag", bagFilePath).group(1)
+    # define the ground truth file name
     groundTruthPath = os.path.dirname(os.getcwd()) + "/ViViD/" + trajectory + "_gt.trc"  
     
     # open the .txt file and grab the lines of output
@@ -76,17 +90,54 @@ def main(*args):
         groundTruth = f1.read().splitlines()
     
     poseGT = [None] * 3 # initialise a list for the ground truth pose graphs
-    
-    # get the position data and extract x, y, z (use ViViD marker 1)
-        # the GT file includes some headers, data starts after the first blank line
+
+    # the GT file includes some headers, data starts after the first blank line
     poseGraphGroundTruth = [line.split() for line in groundTruth[groundTruth.index("") + 1: -1]]
-    # save the start time to subtract from the estimate time
-    startTimeGT = df['header.stamp.secs'][0] + pow(10,-9) * df['header.stamp.nsecs'][0]
-    # now store the timestamps in a list starting from 0.0
-    timestampGT = list(df['header.stamp.secs'] + pow(10,-9) * df['header.stamp.nsecs'] - startTimeGT)
-    poseGT[0] = [float(row[2]) for row in poseGraphGroundTruth] # x1
-    poseGT[1] = [float(row[3]) for row in poseGraphGroundTruth] # y1
-    poseGT[2] = [float(row[4]) for row in poseGraphGroundTruth] # z1
+        
+    # get the time and position data and extract x, y, z    
+    timestampGT = [float(row[1]) for row in poseGraphGroundTruth]
+    poseGT[0] = [(float(row[2]) - float(poseGraphGroundTruth[0][2])) for row in poseGraphGroundTruth] # x1
+    poseGT[1] = [(float(row[3]) - float(poseGraphGroundTruth[0][3])) for row in poseGraphGroundTruth] # y1
+    poseGT[2] = [(float(row[4]) - float(poseGraphGroundTruth[0][4])) for row in poseGraphGroundTruth] # z1
+    
+    # the ground truth is of the markers on top of the setup
+        # we are provided with this extrinsic relation from cam to GT
+    #T_cam_gt = np.array([[0.538117713753331,	0.134273164984585,	-0.832105788532870,	-21.1235087691594],
+    #            [0.440141675139334,	-0.886700487453141,	0.141554058069359,	-17.1621036951045],
+    #            [0.718821696911292,	-0.442417181758784,	-0.536248454854824,	-13.1052654693001],
+    #            [0.0, 0.0, 0.0, 1.0]])
+    
+    rotation_cam_gt = np.array([[0.538117713753331,	0.134273164984585,	-0.832105788532870],
+                                [0.440141675139334,	-0.886700487453141,	0.141554058069359],
+                                [0.718821696911292,	-0.442417181758784,	-0.536248454854824]])
+    
+    translation_cam_gt = np.array([[-21.1235087691594],[-17.1621036951045],[-13.1052654693001]])
+    
+    filler_cam_gt = np.array([0,0,0,1])
+    
+    T_cam_gt = np.vstack((np.hstack((rotation_cam_gt,translation_cam_gt)), filler_cam_gt))
+    
+    rotation_cam_gt_transpose = np.matrix.transpose(rotation_cam_gt)
+    translation_cam_gt_inverseComponent = -1*(rotation_cam_gt_transpose).dot(translation_cam_gt)
+    
+    T_cam_gt_inverse = np.vstack((np.hstack((rotation_cam_gt_transpose,translation_cam_gt_inverseComponent)), filler_cam_gt))
+    
+    # we can then convert the GT from [[x,x..], [y,y..],[z,z..]] to [[x,y,z],[x,y,z],..]
+        # this will allow us to transform each coord by the above matrix
+    xyz = np.array([[poseGT[0][a], poseGT[1][a], poseGT[2][a], 1] for a in range(len(poseGT[0]))])
+    
+    for i in range(len(xyz)):
+        # multiply the inverse of the cam-gt transformation by each gt point
+            # to get the corresponding cam point
+        xyzTranslated = T_cam_gt_inverse @ xyz[i]
+        # the ground truth is in mm, so convert to m here
+        poseGT[0][i] = xyzTranslated[0]/1000
+        poseGT[1][i] = xyzTranslated[1]/1000
+        poseGT[2][i] = xyzTranslated[2]/1000
+    
+    poseGT[0] = [a - poseGT[0][0] for a in poseGT[0]]
+    poseGT[1] = [a - poseGT[1][0] for a in poseGT[1]]
+    poseGT[2] = [a - poseGT[2][0] for a in poseGT[2]]
     
     # ORB-SLAM2 writes the pose estimate to this .txt file
     poseGraphPath = os.getcwd() + "/KeyFrameTrajectory.txt"
@@ -106,31 +157,18 @@ def main(*args):
     
     # format: (0)time, (1)x, (2)y, (3)z, (4)qx, (5)qy, (6)qz, (7)qw
         # orbslam: x,y,z = rightward, downward, forward
-        # vivid coordinate frame agrees with this convention
-    timestampEst = [float(row[0]) - startTimeGT for row in poseGraphEstimate]
+        # vivid camera frame agrees with this
+    timestampEst = [float(row[0]) - startTimeIMG for row in poseGraphEstimate]
     poseEst[0] = [float(row[1]) for row in poseGraphEstimate]
     poseEst[1] = [float(row[2]) for row in poseGraphEstimate]
     poseEst[2] = [float(row[3]) for row in poseGraphEstimate]
     
     if (max(timestampEst) - min(timestampEst))/(max(timestampGT) - min(timestampGT)) < 0.5:
         return(-1,-1)
-    
-    T_cam_gt = [[0.538117713753331,	0.134273164984585,	-0.832105788532870,	-21.1235087691594],
-                [0.440141675139334,	-0.886700487453141,	0.141554058069359,	-17.1621036951045],
-                [0.718821696911292,	-0.442417181758784,	-0.536248454854824,	-13.1052654693001],
-                [0.0, 0.0, 0.0, 1.0]]
-    
-    xyz = np.array([[poseEst[0][a], poseEst[1][a], poseEst[2][a], 1] for a in range(len(poseEst[0]))])
-    
-    for i in range(len(xyz)):
-        xyzTranslated = T_cam_gt @ xyz[i]
-        poseEst[0][i] = xyzTranslated[0]
-        poseEst[1][i] = xyzTranslated[1]
-        poseEst[2][i] = xyzTranslated[2]
-        
-    toTransform = poseEst.copy()
-    toTransform.append([1] * len(poseEst[0]))
-    transformedEstimate = np.matmul(T_cam_gt, toTransform)
+
+    #poseEst[0] = [(a - poseEst[0][0]) for a in poseEst[0]]
+    #poseEst[1] = [(a - poseEst[1][0]) for a in poseEst[1]]
+    #poseEst[2] = [(a - poseEst[2][0]) for a in poseEst[2]]
     
     # get the ground truth timestamps that correspond to the start/end of the estimation
     firstTimestampIdx = timestampGT.index(min(timestampGT, key=lambda x:abs(x-timestampEst[0])))
@@ -139,7 +177,7 @@ def main(*args):
     # truncate the ground truth to the estimation window
     timestampGT = timestampGT[firstTimestampIdx:lastTimestampIdx]
     poseGT = [line[firstTimestampIdx:lastTimestampIdx] for line in poseGT]
-
+    
     # with the truncation, the ground truth will no longer start at [0,0,0]
         # remove the position bias from the truncated ground truth so that the
         # estimation world frame origin and the ground truth world frame origin were
@@ -148,10 +186,29 @@ def main(*args):
     poseGT[1] = [(a - poseGT[1][0]) for a in poseGT[1]]
     poseGT[2] = [(a - poseGT[2][0]) for a in poseGT[2]]
     
+    attitudeTimestamp = list(df2['header.stamp.secs'] + (pow(10,-9)*df2['header.stamp.nsecs']) - (df2['header.stamp.secs'][0] + (pow(10,-9)*df2['header.stamp.nsecs'][0])))
+    firstTimestampIdx = attitudeTimestamp.index(min(attitudeTimestamp, key=lambda x:abs(x-timestampEst[0])))
+    lastTimestampIdx = attitudeTimestamp.index(min(attitudeTimestamp, key=lambda x:abs(x-timestampEst[-1])))
+    
+    initialAttitude = [df2['orientation.w'][firstTimestampIdx], df2['orientation.x'][firstTimestampIdx], df2['orientation.y'][firstTimestampIdx], df2['orientation.z'][firstTimestampIdx]]
+    initialQuaternion = Quaternion(initialAttitude)
+    
+    # rearrange the pose est into [x,y,z][x,y,z].. instead of [x,x,..][y,y,..][z,z,..]
+    xyz = [[poseEst[0][a], poseEst[1][a], poseEst[2][a]] for a in range(len(poseEst[0]))]
+    
+    # the origins are now coincident, but any rotations that happened between
+        # ground truth start time and estimation start time need to be removed
+        # so that at estimation start time the two world frames are aligned
+    for i in range(len(xyz)):
+        rotatedPose = initialQuaternion.rotate(xyz[i])
+        poseEst[0][i] = rotatedPose[0]
+        poseEst[1][i] = rotatedPose[1]
+        poseEst[2][i] = rotatedPose[2]
+    
     # V-SLAM can't get absolute scale. Find the relative scale by looking at
         # the difference in the range of measurements, then scale the estimate
-    #scaleFactor = (max(poseGT[0])-min(poseGT[0]))/(max(poseEst[0])-min(poseEst[0]))
-    #poseEst = [[j*scaleFactor for j in i] for i in poseEst]
+    scaleFactor = (max(poseGT[0])-min(poseGT[0]))/(max(poseEst[0])-min(poseEst[0]))
+    poseEst = [[j*scaleFactor for j in i] for i in poseEst]
     
     # ORB-SLAM2 SVE writes the estimated visibility parameters to this .txt file
     SVEPath = os.getcwd() + "/SceneVisibilityEstimation.txt"
@@ -166,7 +223,7 @@ def main(*args):
     SVE_stats = [None] * 4    # empty array to store visibility estimation
     
     # format (0)timestamp (1)visibility (2)SVE_a (3)SVE_b (4)SVE_c
-    SVE_t = [(float(row[0]) - startTimeGT) for row in SVE_timeSeries]
+    SVE_t = [(float(row[0]) - startTimeIMG) for row in SVE_timeSeries]
     SVE_stats[1] = [float(row[2]) for row in SVE_timeSeries]
     SVE_stats[2] = [float(row[3]) for row in SVE_timeSeries]
     SVE_stats[3] = [float(row[4]) for row in SVE_timeSeries]
